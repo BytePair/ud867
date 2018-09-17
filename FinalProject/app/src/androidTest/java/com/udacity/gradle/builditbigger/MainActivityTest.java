3ddd421e1fa8fb310680858136878b4eb4c8c45d
bytepair <bytepair@gmail.com>
2018-09-17 18:03:56 -0400

    Add joke endpoint

---
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.bytepair.jokedisplay.JokeDisplayActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.bytepair.jokedisplay.JokeDisplayActivity.JOKE_KEY;
+import static org.hamcrest.Matchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void clickJokeButton() {
+        // find the joke button and click it
+        onView(withId(R.id.tell_joke_button)).perform(click());
+
+        // ensure new intent is sent with the expected joke
+        intended(allOf(
+                hasComponent(JokeDisplayActivity.class.getName()),
+                hasExtra(JOKE_KEY, "What do you call a fake noodle? An Impasta.")));
+    }
+
+}
